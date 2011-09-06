d45ed2e17bf122fc1383edbb781120761a31c6c8
optimum7 <optimum7@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-06 19:43:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/google-rank-checker-seo-tool-with-google-api/trunk@434271 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     Description: Provides the estimated Google position for a keyword and corresponding URL. It also provides the competitive pages for any pre-defined keyword list on a keyword for keyword basis.This plugin is an SEO-tool using the 2011 Google API
     Version: 4.0.0
     Author: Optimum7
-    Author URI: http://www.optimum7.com/
+    Author URI: http://www.optimum7.com/?utm_source=RankCheckPlugin
     Copyright 2011  Optimum7 Inc  (email : julian@optimum7.com)
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
